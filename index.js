073debab3aa311efa0c150a35e9d3db847ac4dde
noobhakcer <145267485+noobhakcer@users.noreply.github.com>
2023-10-18 09:36:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
+const cors = require('cors');
+const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
